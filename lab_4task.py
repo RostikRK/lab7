898ec97d76465fec28ceffb3ab3bec0af4fb9ec9
RostikRK <91615985+RostikRK@users.noreply.github.com>
2022-04-03 21:11:28 +0300

    Add files via upload

---
+++ lab_4task.py
@@ -0,0 +1,94 @@
+class Cursor:
+    def __init__(self, document):
+        self.document = document
+        self.position = 0
+
+    def forward(self):
+        self.position += 1
+
+    def back(self):
+        self.position -= 1
+
+    def home(self):
+        while self.document.characters[
+                self.position-1].character != '\n':
+            self.position -= 1
+            if self.position == 0:
+                # Got to beginning of file before newline
+                break
+
+    def end(self):
+        while self.position < len(
+            self.document.characters) and \
+            self.document.characters[
+                self.position
+        ].character != '\n':
+            self.position += 1
+
+
+class WhiteSpaceError(Exception):
+    pass
+
+
+class Document:
+    def __init__(self):
+        self.characters = []
+        self.cursor = Cursor(self)
+        self.filename = ''
+
+    def insert(self, character):
+        if not hasattr(character, 'character'):
+            character = Character(character)
+        self.characters.insert(self.cursor.position,
+                               character)
+        self.cursor.forward()
+
+    def delete(self):
+        del self.characters[self.cursor.position]
+
+    def save(self):
+        f = open(self.filename, 'w')
+        f.write(''.join(self.characters))
+        f.close()
+
+    @property
+    def string(self):
+        return "".join((str(c) for c in self.characters))
+
+
+class Character:
+    def __init__(self, character,
+                 bold=False, italic=False, underline=False):
+        assert len(character) == 1
+        self.character = character
+        self.bold = bold
+        self.italic = italic
+        self.underline = underline
+
+    def check_character(self):
+        if self.character == " ":
+            if (self.bold == True) or (self.italic == True) or (self.underline == True):
+                raise WhiteSpaceError("Whitespace can`t be stylisized")
+
+    def __str__(self):
+        self.check_character()
+        bold = "*" if self.bold else ''
+        italic = "/" if self.italic else ''
+        underline = "_" if self.underline else ''
+        return bold + italic + underline + self.character
+
+
+d = Document()
+d.insert('h')
+d.insert('e')
+d.insert(Character('l', bold=True))
+d.insert(Character('l', bold=True))
+d.insert('o')
+d.insert('\n')
+d.insert(Character('w', italic=True))
+d.insert(Character('o', italic=True))
+d.insert(Character('r', underline=True))
+d.insert(Character(' ', bold=True))
+d.insert('l')
+d.insert('d')
+print(d.string)
