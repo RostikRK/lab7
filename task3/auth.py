7e77a4bf117039bae4b67c4faa8864552abd390c
RostikRK <91615985+RostikRK@users.noreply.github.com>
2022-04-03 21:38:16 +0300

    Update auth.py

--- task3/auth.py
+++ task3/auth.py
@@ -1,1 +1,149 @@
+import hashlib
 
+
+class User:
+    def __init__(self, username, password):
+        '''Create a new user object. The password
+        will be encrypted before storing.'''
+        self.username = username
+        self.password = self._encrypt_pw(password)
+        self.is_logged_in = False
+
+    def _encrypt_pw(self, password):
+        '''Encrypt the password with the username and return
+        the sha digest.'''
+        hash_string = (self.username + password)
+        hash_string = hash_string.encode("utf8")
+        return hashlib.sha256(hash_string).hexdigest()
+
+    def check_password(self, password):
+        '''Return True if the password is valid for this
+        user, false otherwise.'''
+        encrypted = self._encrypt_pw(password)
+        return encrypted == self.password
+
+
+class AuthException(Exception):
+    def __init__(self, username, user=None):
+        super().__init__(username, user)
+        self.username = username
+        self.user = user
+
+
+class UsernameAlreadyExists(AuthException):
+    pass
+
+
+class PasswordTooShort(AuthException):
+    pass
+
+
+class InvalidUsername(AuthException):
+    pass
+
+
+class InvalidPassword(AuthException):
+    pass
+
+
+class PermissionError(Exception):
+    pass
+
+
+class NotLoggedInError(AuthException):
+    pass
+
+
+class NotPermittedError(AuthException):
+    pass
+
+
+class TooShortname(AuthException):
+    pass
+
+
+adminusertest = User("first", "jqwerty")
+
+
+class Authenticator:
+    def __init__(self):
+        '''Construct an authenticator to manage
+        users logging in and out.'''
+        self.users = {"first": adminusertest}
+
+    def add_user(self, username, password):
+        if len(username) < 1:
+            raise TooShortname(username)
+        if username in self.users:
+            raise UsernameAlreadyExists(username)
+        if len(password) < 6:
+            raise PasswordTooShort(username)
+        self.users[username] = User(username, password)
+        authorizor.permit_user("no_permissions", username)
+
+    def login(self, username, password):
+        try:
+            user = self.users[username]
+        except KeyError:
+            raise InvalidUsername(username)
+        if not user.check_password(password):
+            raise InvalidPassword(username, user)
+        user.is_logged_in = True
+        return True
+
+    def is_logged_in(self, username):
+        if username in self.users:
+            return self.users[username].is_logged_in
+        return False
+
+    def delete_user(self, username):
+        if username not in self.users:
+            raise InvalidUsername(username)
+        del self.users[username]
+
+
+class Authorizor:
+    def __init__(self, authenticator):
+        self.authenticator = authenticator
+        self.permissions = {
+            "admin": ["first"],
+            "read_only": [],
+            "write_read": [],
+            "no_permissions": []
+        }
+
+    def permit_user(self, perm_name, username):
+        '''Grant the given permission to the user'''
+        try:
+            perm_set = self.permissions[perm_name]
+        except KeyError:
+            raise PermissionError("Permission does not exist")
+        else:
+            if username not in self.authenticator.users:
+                raise InvalidUsername(username)
+            perm_set.append(username)
+
+    def change_permission(self, new_pername, username):
+        indecator = False
+        for conteiner in self.permissions.values():
+            if username in conteiner:
+                conteiner.remove(username)
+                indecator = True
+        if indecator == False:
+            raise InvalidUsername(username)
+        self.permit_user(new_pername, username)
+
+    def check_permission(self, perm_name, username):
+        if not self.authenticator.is_logged_in(username):
+            raise NotLoggedInError(username)
+        try:
+            perm_set = self.permissions[perm_name]
+        except KeyError:
+            raise PermissionError("Permission does not exist")
+        else:
+            if username in perm_set:
+                return True
+
+
+authenticator = Authenticator()
+authorizor = Authorizor(authenticator)
